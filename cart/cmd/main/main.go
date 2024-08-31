84b6e9adeedf4e45cef419968f57b657f2b613cf
serg14159 <serg301@yandex.ru>
2024-08-31 19:38:32 +0300

    [GOHW-1]: feat: add read config

--- cart/cmd/main/main.go
+++ cart/cmd/main/main.go
@@ -1,9 +1,45 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"route256/cart/internal/config"
+
+	"github.com/joho/godotenv"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
-	fmt.Print("Hello world!")
+	_ = godotenv.Load()
+
+	// Read flag
+	var configPath string
+	flag.StringVar(&configPath, "config", "", "Path to config file")
+	flag.Parse()
+
+	if configPath == "" {
+		configPath = "config.yml"
+	}
+
+	// Read config
+	cfg := config.NewConfig()
+	if err := cfg.ReadConfig(configPath); err != nil {
+		log.Fatal().Err(err).Msg("Failed init configuration")
+	}
+
+	log.Info().
+		Str("version", cfg.Project.Version).
+		Str("commitHash", cfg.Project.CommitHash).
+		Bool("debug", cfg.Project.Debug).
+		Str("environment", cfg.Project.Environment).
+		Msgf("Starting service: %s", cfg.Project.Name)
+
+	// Set log level
+	if cfg.Project.Debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	} else {
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	}
+
+	log.Debug().Msgf("Cfg: %v", cfg)
 }
---
+++ cart/internal/config/config.go
@@ -0,0 +1,150 @@
+package config
+
+import (
+	"github.com/rs/zerolog/log"
+
+	"github.com/spf13/viper"
+)
+
+// Build information -ldflags.
+var (
+	version    string = "dev"
+	commitHash string = "-"
+)
+
+// Project - contains all parameters project information.
+type Project struct {
+	Debug       bool   `yaml:"debug" mapstructure:"debug"`
+	Name        string `yaml:"name" mapstructure:"name"`
+	Environment string `yaml:"environment" mapstructure:"environment"`
+	Version     string
+	CommitHash  string
+}
+
+func (p *Project) GetDebug() bool {
+	return p.Debug
+}
+
+func (p *Project) GetName() string {
+	return p.Name
+}
+
+func (p *Project) GetEnvironment() string {
+	return p.Environment
+}
+
+func (p *Project) GetVersion() string {
+	return p.Version
+}
+
+func (p *Project) GetCommitHash() string {
+	return p.CommitHash
+}
+
+// Server - contains parameters for server address and port
+type Server struct {
+	Host string `yaml:"host" mapstructure:"host"`
+	Port string `yaml:"port" mapstructure:"port"`
+}
+
+func (s *Server) GetPort() string {
+	return s.Port
+}
+
+func (s *Server) GetHost() string {
+	return s.Host
+}
+
+// ProductService - contains parameters for ProductService.
+type ProductService struct {
+	ApiURI string `yaml:"apiuri" mapstructure:"apiuri"`
+	Token  string `yaml:"token" mapstructure:"token"`
+}
+
+func (ps *ProductService) GetURI() string {
+	return ps.ApiURI
+}
+
+func (ps *ProductService) GetToken() string {
+	return ps.Token
+}
+
+// Config - contains all configuration parameters in config package
+type Config struct {
+	Project        Project        `yaml:"project" mapstructure:"project"`
+	Server         Server         `yaml:"server" mapstructure:"server"`
+	ProductService ProductService `yaml:"productService" mapstructure:"productService"`
+}
+
+func NewConfig() *Config {
+	return &Config{}
+}
+
+// ReadConfig - read configurations from default/file/env and init instance Config.
+func (c *Config) ReadConfig(configPath string) error {
+
+	// Set default
+	// Project
+	viper.SetDefault("project.debug", "false")
+	viper.SetDefault("project.name", "Cart")
+	viper.SetDefault("project.environment", "development")
+	// Server
+	viper.SetDefault("server.port", "8082")
+	viper.SetDefault("server.host", "localhost")
+	// ProductService
+	viper.SetDefault("productService.apiuri", "http://route256.pavl.uk:8080")
+	viper.SetDefault("productService.token", "testtoken")
+
+	// Read config file
+	viper.SetConfigFile(configPath)
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Error().Msgf("Error loading config file: %v", err)
+	}
+
+	// Read env
+	viper.AutomaticEnv()
+	// Project
+	err = viper.BindEnv("project.debug", "PROJECT_DEBUG")
+	if err != nil {
+		log.Error().Msgf("Error bind env: %v", err)
+	}
+	err = viper.BindEnv("project.name", "PROJECT_NAME")
+	if err != nil {
+		log.Error().Msgf("Error bind env: %v", err)
+	}
+	err = viper.BindEnv("project.environment", "PROJECT_ENVIRONMENT")
+	if err != nil {
+		log.Error().Msgf("Error bind env: %v", err)
+	}
+	// Server
+	err = viper.BindEnv("server.host", "SERVER_HOST")
+	if err != nil {
+		log.Error().Msgf("Error bind env: %v", err)
+	}
+	err = viper.BindEnv("server.port", "SERVER_PORT")
+	if err != nil {
+		log.Error().Msgf("Error bind env: %v", err)
+	}
+	// ProductService
+	err = viper.BindEnv("productService.apiuri", "PRODUCT_SERVICE_APIURI")
+	if err != nil {
+		log.Error().Msgf("Error bind env: %v", err)
+	}
+	err = viper.BindEnv("productService.token", "PRODUCT_SERVICE_TOKEN")
+	if err != nil {
+		log.Error().Msgf("Error bind env: %v", err)
+	}
+
+	// Load config into struct
+	err = viper.Unmarshal(&c)
+	if err != nil {
+		log.Error().Msgf("Error unmarshalling config: %v", err)
+		return err
+	}
+
+	c.Project.Version = version
+	c.Project.CommitHash = commitHash
+
+	return nil
+}
