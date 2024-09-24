d69781f3036439b25a7937d7ed7dee3d487a2b70
serg14159 <serg301@yandex.ru>
2024-09-24 04:31:46 +0300

    [GOHW-4]: feat: add package database for create db connect

--- loms/go.mod
+++ loms/go.mod
@@ -20,6 +20,14 @@ require (
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.3 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgtype v1.14.0 // indirect
+	github.com/jackc/pgx/v4 v4.18.3 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
@@ -33,6 +41,7 @@ require (
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
+	golang.org/x/crypto v0.24.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
---
+++ loms/internal/pkg/db/db.go
@@ -0,0 +1,53 @@
+package database
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v4"
+)
+
+type Config interface {
+	GetDSN() string // Data Source Name
+}
+
+// Function NewConnect create new connection to DB
+func NewConnect(ctx context.Context, cfg Config) (*pgx.Conn, error) {
+	conn, err := pgx.Connect(ctx, cfg.GetDSN())
+	if err != nil {
+		return nil, fmt.Errorf("pgx.Connect() err: %w", err)
+	}
+
+	// Check connect
+	err = conn.Ping(ctx)
+	if err != nil {
+		conn.Close(ctx)
+		return nil, fmt.Errorf("conn.Ping() err: %w", err)
+	}
+
+	return conn, nil
+}
+
+// WithTxFunc type of function for performs a function inside a transaction
+type WithTxFunc func(ctx context.Context, tx pgx.Tx) error
+
+// Function WithTx performs a function inside a transaction
+func WithTx(ctx context.Context, conn *pgx.Conn, fn WithTxFunc) error {
+	tx, err := conn.Begin(ctx)
+	if err != nil {
+		return fmt.Errorf("conn.Begin() err: %w", err)
+	}
+
+	if err := fn(ctx, tx); err != nil {
+		if rbErr := tx.Rollback(ctx); rbErr != nil {
+			return fmt.Errorf("tx.Rollback() err: %w", err)
+		}
+		return fmt.Errorf("fn(ctx, tx) err: %w", err)
+	}
+
+	if err := tx.Commit(ctx); err != nil {
+		return fmt.Errorf("tx.Commit() err: %w", err)
+	}
+
+	return nil
+}
